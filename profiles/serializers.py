739a250754650fdee09961ad7a0b092463f2fc91
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-21 16:11:07 +0000

    add login/logout views and permissions

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,11 +3,17 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'writer', 'artist', 'letterer','editor'
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 'writer', 'artist', 'letterer','editor', 'is_owner'
         ]
 
 
