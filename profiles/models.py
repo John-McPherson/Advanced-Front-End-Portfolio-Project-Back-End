b763716707252b49cc5c50d6404c3a7fb287cf83
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-21 13:03:14 +0000

    add signal to profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+
+
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -19,4 +22,11 @@ class Profile(models.Model):
         ordering = ['-created_at']
     def __str__(self):
         return f"{self.owner}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+        
+post_save.connect(create_profile, sender=User)
+
 
