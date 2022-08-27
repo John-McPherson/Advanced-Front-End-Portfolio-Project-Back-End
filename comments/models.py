13a3ab067191971f52cb591a8fcc81bfcdcb4caf
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 16:48:34 +0000

    add comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from pages.models import Page
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model, related to User and Page
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.content
