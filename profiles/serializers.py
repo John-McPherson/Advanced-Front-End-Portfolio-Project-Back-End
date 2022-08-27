ef1f3e092cf6c60d72e99bdd76e42fb3350c9d62
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 16:19:55 +0000

    add image validation to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,6 +6,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 124 *1024 * 2
+            raise serializers.ValidationError(
+                'Image size larger than 2MB.'
+            )
+        if value.image.width > 2063:
+            raise serializers.ValidationError(
+                'Image width larger than 4000px.'
+            )
+        if value.image.height > 3131:
+            raise serializers.ValidationError(
+                'Image height larger than 4000px.'
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
