8c722056409937f68d29e026f396cc4bd4900feb
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 18:03:15 +0000

    add jwt samesite settings

--- makecomics_api/settings.py
+++ makecomics_api/settings.py
@@ -44,11 +44,10 @@ if 'DEV' not in os.environ:
 
 
 REST_USE_JWT = True
-
 JWT_AUTH_COOKIE = 'my-app-auth'
-
 JWT_AUTH_SECURE = True
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'makecomics_api.serializers.CurrentUserSerializer'}
 
@@ -84,7 +83,6 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
-    "corsheaders",
     "rest_framework",
     'django_filters',
     'rest_framework.authtoken', 
@@ -94,6 +92,7 @@ INSTALLED_APPS = [
     'allauth.account', 
     'allauth.socialaccount', 
     'dj_rest_auth.registration',
+    "corsheaders",
 
 
     "profiles",
