18114b94fd43bddc2eaf742d52eeca0d33385a61
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 11:06:31 +0000

    add test cases for project list view

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='john', password='pass')
+
+    def test_can_list_projects(self):
+        john = User.objects.get(username='john')
+        Project.objects.create(owner=john, title='a title')
+        response = self.client.get('/project/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data, len(response.data))
+
+    def test_logged_in_user_can_create_project(self):
+        self.client.login(username='john', password='pass')
+        response = self.client.post('/project/', {'title': 'a title'})
+        count = Project.objects.count()
+        self.assertEqual(count, 1)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_logged_out_user_cant_create_project(self):
+        response = self.client.post('/project/', {'title': 'a title'})
+        count = Project.objects.count()
+        self.assertEqual(count, 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+
+
