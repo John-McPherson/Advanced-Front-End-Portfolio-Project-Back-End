240b580233b0cb681bd808d811189761e509248f
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 17:53:52 +0000

    fix defaultrenderer  causing deployment to fail

--- makecomics_api/settings.py
+++ makecomics_api/settings.py
@@ -38,7 +38,7 @@ REST_FRAMEWORK = {
     }
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
-        'restframework.renderers.JSONRenderer'
+        'rest_framework.renderers.JSONRenderer'
     ]
 
 
