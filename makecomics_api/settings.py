b4839ea51665e0461ede6017457fae59aa66c11e
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-27 17:30:44 +0000

    add jwt and prep for deployment

--- makecomics_api/settings.py
+++ makecomics_api/settings.py
@@ -26,6 +26,23 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [( 
+        'rest_framework.authentication.SessionAuthentication' 
+        if 'DEV' in os.environ 
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+   )]
+    }
+
+REST_USE_JWT = True
+
+JWT_AUTH_COOKIE = 'my-app-auth'
+
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+
+REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'makecomics_api.serializers.CurrentUserSerializer'}
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -60,12 +77,23 @@ INSTALLED_APPS = [
     "cloudinary",
     "corsheaders",
     "rest_framework",
-     'django_filters',
+    'django_filters',
+    'rest_framework.authtoken', 
+    'dj_rest_auth', 
+    'django.contrib.sites', 
+    'allauth', 
+    'allauth.account', 
+    'allauth.socialaccount', 
+    'dj_rest_auth.registration',
+
+
     "profiles",
     "project",
     "pages",
     'comments',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
