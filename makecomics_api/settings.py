080e60e44b538052d6bf6cb49f3bf32fdaca4ad9
John-McPherson <john-mac1989@hotmail.co.uk>
2022-08-29 10:42:09 +0000

    remove samesite

--- makecomics_api/settings.py
+++ makecomics_api/settings.py
@@ -44,10 +44,10 @@ if 'DEV' not in os.environ:
 
 
 REST_USE_JWT = True
-JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_SECURE = True
+JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-JWT_AUTH_SAMESITE = 'None'
+# JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {'USER_DETAILS_SERIALIZER': 'makecomics_api.serializers.CurrentUserSerializer'}
 
