d666e050c5a137d3df2f6941b2b886ba5f60fad9
John-McPherson <john-mac1989@hotmail.co.uk>
2022-09-26 14:49:23 +0000

    add delete profile view

--- profiles/views.py
+++ profiles/views.py
@@ -3,11 +3,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status , permissions
 from .models import Profile
 from .serializers import ProfileSerializer
 from makecomics_api.permissions import IsOwnerOrReadOnly
-
+from django.contrib.auth.models import User
 
 class ProfileList(generics.ListAPIView):
     queryset = Profile.objects
@@ -49,3 +49,35 @@ class ProfileDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileDelete(APIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile, context={"request": request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, data=request.data, context={"request": request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = User.objects.filter(id=pk)
+        print(user)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
